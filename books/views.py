a08304760177e0e06b04427905f45253f4949bd9
ILGIZ_AKBARALIEV <okoo.goe@gmail.com>
2025-02-07 03:38:37 +1100

    0.4

--- books/views.py
+++ books/views.py
@@ -8,16 +8,16 @@ def books_list_view(request):
     if request.method == "GET":
         query = models.Books.objects.all().order_by('-id')
         context_object_name = {
-            'books_list_view': query,
+            'books_list': query,
         }
-        return render(request, template_name = 'book_detail.htm',
+        return render(request, template_name = 'book.html',
                       context= context_object_name)
 
 def books_detail_view(request, id):
     if request.method == "GET":
         query = get_object_or_404(models.Books, id=id)
         context_object_name = {
-            'books_list_detail': query,
+            'book_list_id': query,
         }
         return render(request,
                       template_name = 'book_detail.html',
